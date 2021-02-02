9398eb308b79d2a32a5e5fe41f588dcf36e4ffe3
Matt Wong <MattWong@Matts-MacBook-Pro-2.local>
2021-02-02 15:41:24 -0500

    Prepare chnaged ip address for final production

--- config.js
+++ config.js
@@ -38,8 +38,8 @@ module.exports = {
             {
               // ip: "0.0.0.0",
               // When the ports go public, we need to fill in the announcedIp with the public server domain
-              ip: "192.168.83.129",
-              announcedIp: null,
+              ip: "0.0.0.0",
+              announcedIp: "mediasoup.umiacs.umd.edu",
             }
           ],
           maxIncomingBitrate: 1500000,
